bf13e3ec861d0e88813ee0a47122e1f69a47b968
Allan <allan.sduarte@gmail.com>
2021-01-17 16:08:45 -0300

    Add adjacent elements product solution

---
+++ code-signal-solutions/adjacentElementsProduct.js
@@ -0,0 +1,16 @@
+function adjacentElementsProduct(inputArray) {
+    if (inputArray.length < 2 || inputArray.length > 10) return;
+
+    let largestAdjacentElements = [null, null, null];
+    for (let i = 1; i < inputArray.length; i++) {
+        if (inputArray[i] < -1000 || inputArray[i] > 1000) return;
+
+        if (!largestAdjacentElements[2] || inputArray[i - 1] * inputArray[i] > largestAdjacentElements[2]) {
+            largestAdjacentElements[0] = inputArray[i - 1];
+            largestAdjacentElements[1] = inputArray[i];
+            largestAdjacentElements[2] = inputArray[i] * inputArray[i - 1];
+        }
+    }
+
+    return largestAdjacentElements[2];
+}
