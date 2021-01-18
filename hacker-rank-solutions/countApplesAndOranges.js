f1dabaf91351d30288812795b6751b7047953fc4
Allan <allan.sduarte@gmail.com>
2021-01-18 10:45:42 -0300

    Add count apples and oranges solution

---
+++ hacker-rank-solutions/countApplesAndOranges.js
@@ -0,0 +1,15 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    console.log(countApplesAndOrangesHelper(apples, a, s, t));
+    console.log(countApplesAndOrangesHelper(oranges, b, s, t));
+}
+
+function countApplesAndOrangesHelper(fruits, treePosition, initialPositionHouse, finalPositionHouse) {
+    let counter = 0;
+    for (let i = 0; i < fruits.length; i++) {
+        const fruitLandedPosition = treePosition + fruits[i];
+        if (fruitLandedPosition >= initialPositionHouse && fruitLandedPosition <= finalPositionHouse) {
+            counter++;
+        }
+    }
+    return counter;
+}
