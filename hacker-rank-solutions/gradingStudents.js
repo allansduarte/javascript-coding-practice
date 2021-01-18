92bfe0bdbff765db62479c86a3928b0e18fa963b
Allan <allan.sduarte@gmail.com>
2021-01-18 09:29:52 -0300

    Add grading students solution

---
+++ hacker-rank-solutions/gradingStudents.js
@@ -0,0 +1,12 @@
+function gradingStudents(grades) {
+    for (let i = 0; i < grades.length; i++) {
+        const gradeDifference = 5 - (grades[i] % 5);
+        if (grades[i] < 38) continue;
+
+        if (gradeDifference < 3) {
+            grades[i] = grades[i] + gradeDifference;
+        }
+    }
+
+    return grades;
+}
